c9caa9957c566d675542fc79daeb75ed220e8221
stingtree <82956002+stingtree@users.noreply.github.com>
2021-04-22 21:37:19 +0800

    Add files via upload

--- error_handle/sqlNoRows.go
+++ error_handle/sqlNoRows.go
@@ -1,35 +1,36 @@
 package main
 
 import (
-	"database/sql"
-	"errors"
+	sql2 "database/sql"
 	"fmt"
-	"time"
+	"github.com/pkg/errors"
 )
 
-type QueryError struct {
-	Err   error
+type QueryRows struct {
 	query string
+	err   error
 }
 
-//数据查询函数
 func query(query string) error {
-	err := &QueryError{
-		Err:   sql.ErrNoRows,
+	sql := &QueryRows{
 		query: query,
+		err:   sql2.ErrNoRows,
 	}
-	//将执行失败的原因、携带执行语句以及执行事件一并返回给函数调用方,便于调用方进行后续其他业务逻辑处理
-	err_s := fmt.Errorf("sql执行语句为: %s\n执行报错原始信息是：%w\n执行时间：%v\n", err.query, err.Err,time.Now().Unix())
-	return err_s
+	err := errors.WithMessagef(sql.err, "NoRowsFound with %s", sql.query)
+	return err
 }
 
 func main() {
-	sql_exec := "select name from user_info"
-	err := query(sql_exec)
-	fmt.Println(err)
-	if errors.Is(errors.Unwrap(err), sql.ErrNoRows) {
-		fmt.Println(errors.Unwrap(err))
-	} else {
-		fmt.Println("This error is not sql errors,the details: %v", err)
+	query_string := "select name from user_info"
+	err := query(query_string)
+	//print stack,lock the error occurred line
+	fmt.Printf("%+v \n", errors.WithStack(err))
+	err_cause := errors.Cause(err)
+	//measure affairs according to error cause
+	switch err_cause {
+	case sql2.ErrNoRows:
+		println("NoRows")
+	default:
+		println("Learing Go")
 	}
 }
